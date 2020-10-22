a22381d1a0ab2cb45a4787339238d58e63464549
NicolasRoche <nicolas8roche@gmail.com>
2020-10-22 13:16:18 +0200

    API Key (todo extract)

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,18 @@ import axios from "axios";
 const baseUrl = "https://gateway.marvel.com:443/v1/public/"
 "https://gateway.marvel.com:443/v1/public/characters/1?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669"
 
+const apikey = 'c37ea365e6a1a371ef21f714e01af669'; 
+const hash = '8ffd6a155254bc99459933656517cf50&ts=timestamp'; 
+
 export default {
     marvel(url = baseUrl) {
         return {
-            fetchAllComics: () => axios.get(url+"comics?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669&limit=40"),
-            fetchByIdComic: id => axios.get(url+"comics/"+ id+"?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669"),
-            fetchByIdCharactersOfComic: id => axios.get(url+"comics/"+ id+"/characters?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669"),
-            fetchAllCharacters: () => axios.get(url +"characters?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669&limit=40"),
-            fetchByIdCharacter: id => axios.get(url +"characters/"+ id +"?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669"),
-            fetchByIdComicsOfCharacter: id => axios.get(url+"characters/"+ id+"/comics?hash=8ffd6a155254bc99459933656517cf50&ts=timestamp&apikey=c37ea365e6a1a371ef21f714e01af669")
+            fetchAllComics: () => axios.get(url+"comics?hash="+hash+"&apikey="+apikey+"&limit=40"),
+            fetchByIdComic: id => axios.get(url+"comics/"+ id+"?hash="+hash+"&apikey="+apikey+""),
+            fetchByIdCharactersOfComic: id => axios.get(url+"comics/"+ id+"/characters?hash="+hash+"&apikey="+apikey+""),
+            fetchAllCharacters: () => axios.get(url +"characters?hash="+hash+"&apikey="+apikey+"&limit=100"),
+            fetchByIdCharacter: id => axios.get(url +"characters/"+ id +"?hash="+hash+"&apikey="+apikey+""),
+            fetchByIdComicsOfCharacter: id => axios.get(url+"characters/"+ id+"/comics?hash="+hash+"&apikey="+apikey+"")
         }
     }
 }
